48fb666c0d3b371a664b8634e62c683ef6781322
medinammartin3 <medinammartin3@gmail.com>
2025-11-21 22:33:10 -0500

    Change locations

--- path_planning.py
+++ path_planning.py
@@ -340,12 +340,12 @@ def get_path_and_speed_limits(route):
 
 if __name__ == '__main__':
     # Route locations
-    start = 'Avenue Lincoln 1680, H3H 1G9 Montréal, Québec, Canada'
-    end = 'Tim Hortons, Rue Guy 2081, H3H 2L9 Montréal, Québec, Canada'
+    # start = 'Avenue Lincoln 1680, H3H 1G9 Montréal, Québec, Canada'
+    # end = 'Tim Hortons, Rue Guy 2081, H3H 2L9 Montréal, Québec, Canada'
     # start = 'McGill University'
     # end = 'Université de Montréal'
-    # start = 'Musée des Beaux-Arts de Montréal'
-    # end = 'Concordia University (SGW Campus)'
+    start = 'Musée des Beaux-Arts de Montréal'
+    end = 'Concordia University (SGW Campus)'
     locations_list = [start, end]
     vehicle = 'car'  # ['car', 'truck']
 
